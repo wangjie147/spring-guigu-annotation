f7dc88d03e7c5c886cc23eb64d1bed98c5c5976d
WILLing <wangle1988918@163.com>
2018-09-17 13:20:22 +0800

    init

--- spring-annotation/src/main/java/com/facetofront/config/MainConfig.java
+++ spring-annotation/src/main/java/com/facetofront/config/MainConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 import com.facetofront.domain.Person;
 //配置类==配置文件
@@ -14,10 +15,40 @@ import com.facetofront.domain.Person;
 //@ComponentScan(value="com.facetofront"，excludeFilters={@Filter(type=FilterType.ANNOTATION,classes={Controller.class,Service.class})} ) value :指定要扫描的包   excludeFilters：排除要扫描的包
 //@includeFilters(value="com.facetofront"，excludeFilters={@Filter(type=FilterType.ANNOTATION,classes={Controller.class,Service.class})},userDefaulters=false )
 //@ComponentScan是重复注解，如果是jdk8的话，可以使用多次，一个类中，@Repeatable(ComponentScans.class)，如果是jdk1.7的话可以使用@ComponentScans指定扫描策略，里面的value是重复数组。
+//FilterType.ANNOTATION:按照注解
+//FilterType.ASSIGNABLE_TYPE:按照给定的类型
+//FilterType.ASPECTJ：使用ASPECTJ表达式
+//FilterType.REGEX：使用正则指定
+//FilterType.CUSTOM：使用自定义规则   @Filter(type=FilterType.CUSTOM,classes={MyTypeFilter.class})
 public class MainConfig {
       
+	
+	  /**
+	   *  懒加载：容器启动的时候，不创建对象，第一次使用bean的时候，创建对象，并初始化。
+	   *      单实例bean；默认在容器启动的时候创建对象
+	   * @return
+	   */
+	  //@Lazy
 	  @Bean("person")
 	  public Person person01(){
 		  return new Person("lili",20);
 	  }
+	  
+	  
+	  /**
+	  * @conditional({}):按照一定的条件进行哦按段，满足条件给容器中注册bean
+	  * 
+	  * 如果系统是windows，放bill，如果是linux,放linux
+	  * 
+	  */
+	 @Bean("bill") 
+	 public Person person1(){
+		 return new Person("Bill Gates",62);
+	 }
+	 
+	 
+	 @Bean("linux") 
+	 public Person person2(){
+		 return new Person("linux",62);
+	 }
 }
--- spring-annotation/src/test/java/com/facetofront/test/IocTest.java
+++ spring-annotation/src/test/java/com/facetofront/test/IocTest.java
@@ -1,9 +1,13 @@
 package com.facetofront.test;
 
+import java.util.Map;
+
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 import com.facetofront.config.MainConfig;
+import com.facetofront.domain.Person;
 
 public class IocTest {
 	
@@ -19,5 +23,28 @@ public class IocTest {
 		  }
 		  
 	  }
+	  
+	  @Test
+	  public void test02(){
+		  
+		  AnnotationConfigApplicationContext annotationConfigApplicationContext=new AnnotationConfigApplicationContext(MainConfig.class);
+		  
+		  String[] definitionNames = annotationConfigApplicationContext.getBeanDefinitionNames();
+		  
+		 /* for(String definitionName:definitionNames){
+			  System.out.println(definitionName);
+		  }*/
+		  ConfigurableEnvironment environment = annotationConfigApplicationContext.getEnvironment();
+		  String property = environment.getProperty("os.name");
+		  System.out.println(property);
+		  
+		  Map<String, Person> persons = annotationConfigApplicationContext.getBeansOfType(Person.class);
+		  
+		  System.out.println(persons);
+		  
+	  }
+	  
+	  
+	  
 
 }
