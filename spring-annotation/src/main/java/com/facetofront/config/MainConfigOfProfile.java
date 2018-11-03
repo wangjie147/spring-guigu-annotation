9d3bc8f592232f54048af11ee39603c80cdeea31
WILLing <wangle1988918@163.com>
2018-11-03 15:47:38 +0800

    2018.11.3

--- spring-annotation/src/main/java/com/facetofront/config/MainConfigOfProfile.java
+++ spring-annotation/src/main/java/com/facetofront/config/MainConfigOfProfile.java
@@ -9,13 +9,27 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.EmbeddedValueResolverAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.util.StringValueResolver;
 
+import com.facetofront.domain.Yellow;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
-
-
+/**
+ * 
+ * @Profile:指定组件在哪个环境的情况下才能被注册到容器中，不指定：任何环境下都能注册
+ * 这个组件
+ * 
+ *  @Profile("default") 
+ *  1、加了环境表示的bean，只有这个环境被激活的时候才能注册到容器里面
+ *  默认是default环境
+ *  2、写在配置类上面@Profile("test")，只有是指定的环境的时候 ，整个配置类里面的所有的配置才能开始生效
+ *  3、没有标注环境表示的bean在任何环境下都是加载的
+ * @author wangl
+ *
+ */
+//@Profile("test")
 @PropertySource("classpath:/dbconfig.properties")
 @Configuration
 public class MainConfigOfProfile implements EmbeddedValueResolverAware{
@@ -27,9 +41,17 @@ public class MainConfigOfProfile implements EmbeddedValueResolverAware{
 	  
 	  private String resolveStringValue ;
 	  
+	  @Profile("test")
+	  @Bean
+	  public Yellow yellow(){
+		  return new Yellow();
+	  }
+	  
 	  /**
 	   * @throws PropertyVetoException 
 	   */ 
+	  
+	   @Profile("test")
 	   @Bean("testData")
 	   public DataSource dataSourceTest(@Value("${db.driverClass}")String url) throws PropertyVetoException{
 		   ComboPooledDataSource dataSource = new ComboPooledDataSource();
@@ -39,7 +61,7 @@ public class MainConfigOfProfile implements EmbeddedValueResolverAware{
 		   dataSource.setDriverClass(url);
 		   return null;
 	   }
-	   
+	   @Profile("dev")
 	   @Bean("adminData")
 	   public DataSource dataSourceDev(@Value("${db.driverClass}")String url) throws PropertyVetoException{
 		   ComboPooledDataSource dataSource = new ComboPooledDataSource();
@@ -49,7 +71,7 @@ public class MainConfigOfProfile implements EmbeddedValueResolverAware{
 		   dataSource.setDriverClass(url);
 		   return null;
 	   }
-	   
+	   @Profile("prod")
 	   @Bean("taotaoData")
 	   public DataSource dataSourceProd(@Value("${db.driverClass}")String url) throws PropertyVetoException{
 		   ComboPooledDataSource dataSource = new ComboPooledDataSource();
--- spring-annotation/src/test/java/com/facetofront/test/IOCTest_Profile.java
+++ spring-annotation/src/test/java/com/facetofront/test/IOCTest_Profile.java
@@ -9,8 +9,9 @@ import com.facetofront.config.MainConfigOfProfile;
 
 public class IOCTest_Profile {
 	
-	  @Test
-	  public void test01(){
+	  //1、使用命令行动态参数：在虚拟机参数的位置加载-Dspring.profiles.active=test
+	  /* @Test
+	     public void test01(){
 		  //创建ioc容器
 		  AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfigOfProfile.class);
 		  String[] beanNamesForType = applicationContext.getBeanNamesForType(DataSource.class);
@@ -18,6 +19,36 @@ public class IOCTest_Profile {
 			  System.out.println(beanNamesForType[i]);
 		  }
 	      applicationContext.close();
+	  }*/
+	  
+	
+	 //2\代码的方式激活某种环境
+	  @Test
+	  public void test01(){
+		  //1、创建一个ApplicationContext的
+		  AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+		  //2、设置需要激活的环境
+		  applicationContext.getEnvironment().setActiveProfiles("test","dev");
+		  //3、注册主配置类
+		  applicationContext.register(MainConfigOfProfile.class);
+		  //4、启动刷新容器
+		  applicationContext.refresh();
+		  
+		  /*
+		   * 有参构造器
+		   * 
+		  public AnnotationConfigApplicationContext(Class<?>... annotatedClasses) {
+				this();无参创建对象
+				register(annotatedClasses);注册
+				refresh();刷新
+			}*/
+		  String[] beanNamesForType = applicationContext.getBeanNamesForType(DataSource.class);
+		  for(int i=0;i<beanNamesForType.length;i++){
+			  System.out.println(beanNamesForType[i]);
+		  }
+	      applicationContext.close();
+	      
+	      
 	  
 	  }
 
